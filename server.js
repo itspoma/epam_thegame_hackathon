b6863fba3505b56c3a72c900fa856009b895eb11
TuxujPes <tuxuj.pes@yandex.ru>
2013-04-11 22:20:21 +0300

    remove unnecessary files

--- server.js
+++ server.js
@@ -20,36 +20,11 @@ app.configure(function(){
   app.use(app.router);
 });
 
-//game
-//store users
-var users = [];
-var currentUserTurn = 0;
-
 io.sockets.on("connection", function(socket) {
   console.log('CONNECTION');
-  socket.on('join', function(nick, callback) {
-    console.log('smn want to connect : ' + nick)
-    // If the nickname isn't in use, join the user
-    if (users.indexOf(nick) < 0) {
-      users.push(nick);
-
-      socket.broadcast.emit("user-joined", nick);
-      if (users.length > 1) {
-        //game can be started, with first one to login to start
-        console.log('GAME START: first turn - ' + users[currentUserTurn])      
-        socket.broadcast.emit("turnStart", {user: users[currentUserTurn]});
-      }
-      callback(true, users);
-    } else {
-      callback(false);
-    }
-  });
-  socket.on('passTurn', function(point){
-    console.log('PASS TURN from ' + users[currentUserTurn]); 
-    currentUserTurn = currentUserTurn === 0 ? 1 : 0;
-    console.log('PASS TURN to ' + users[currentUserTurn]);
-    socket.broadcast.emit("turnStart", {user: users[currentUserTurn], point : point});
-  })
+  socket.on('join', function(user, callback) {
+    console.log('smn want to connect : ' + user);    
+  });  
 });
 
 server.listen(port);
