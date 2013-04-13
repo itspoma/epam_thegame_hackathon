2f5cf3fc6812307d071bc6cd70a7aab26c784aad
Sviatoslav Zalishchuk <scroll.ua@gmail.com>
2013-04-13 22:03:19 +0300

    added turn by turn gameplay

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -12,29 +12,46 @@ $(function(){
       $('#container table td span').on('click', function(){
         var x = $(this).attr('data-x');
         var y = $(this).attr('data-y');
+        app.helpers.makeTurn(x, y);
       });
-      cachedEls.$play.on('click', function() {
-        cachedEls.$login
-          .animate(
-            { left: '-2000px', opacity: 0 },
-            { duration: 1200, easing: 'linear', complete: function() {
-                cachedEls.$login.hide();
-                cachedEls.$game.show().animate(
-                  { left: '0', opacity: 1 },
-                  { duration: 600, easing: 'linear', complete: function() {
-                    app.startGame();
-                  }}
-                );
-              }
-            }
+      //Launch connection process
+      cachedEls.$play.on('click', function() { cachedEls.$body.trigger('connect'); });
+      //Connect two players through lobby
+      cachedEls.$body.on('connect', function() {
+        console.log('connect!');
 
-        );
+        app.properties.socket = io.connect('http://localhost/');
+        console.log(app.properties.socket);
+
+        app.properties.socket.on('connected', function(client) {
+          app.properties.userId = client.id;
+          console.log(client.id + ' is connected');
+        });
+        app.properties.socket.on('gameReadyStatus', function(gameIsReady) {
+          if (gameIsReady) {
+            console.log('START GAME');
+            app.pages.game.initPage();
+            app.properties.socket.emit('clientReadyForGame', { id: app.properties.userId });
+          } else { 
+            console.log('WAIT for RIVAL');
+            app.pages.lobby.initPage();
+          }
+        });
+        app.properties.socket.on('startTurn', function(gameData){
+          var playerTurnID = gameData.player.id;
+          console.log('TURN STARTED: player turn - ' + playerTurnID);
+          if(app.properties.userId === playerTurnID) {
+            setTimeout( function() { app.helpers.makeTurn(); }, 5000);
+          }
+        });
       });
     },
     cachedEls: {
       initCache: function() {
+        this.$body = $('body');
         this.$login = $('.login');
         this.$game = $('.game');
+        this.$lobby = $('.lobby');
         this.$play = $('.btn.play');
       }
     },
@@ -44,7 +61,34 @@ $(function(){
       boardWidth: 10,
       boardHeight: 7
     },
+    properties: {
+      currentPage: $('section[id^="pagename-"]').filter(function () { return $(this).hasClass('active'); }),
+      socket: '',
+      userId: ''
+    },
     helpers: {
+      switchPageTo: function(selector) {
+        var nextPage = selector;
+        var currentPage = app.properties.currentPage;
+        currentPage.animate(
+            { left: '-2000px', opacity: 0 },
+            { duration: 1200, easing: 'linear', complete: function() {
+                currentPage.removeClass('active').hide();
+                selector.show().animate(
+                  { left: '0', opacity: 1 },
+                  { duration: 600, easing: 'linear', complete: function() {
+                      selector.addClass('active'); app.properties.currentPage = selector; }
+                  }
+                );
+              }
+            }
+        );
+      },
+      makeTurn: function(xIndex, yIndex) {
+        var data = { x : xIndex, y : yIndex };
+        app.properties.socket.emit('playerMadeTurn', data);
+        console.log('TURN ENDED');
+      },
       drawTable: function() {
         $('#container').append($('<table/>'));
         for (var x=1; x<=app.settings.boardHeight; x++) {
@@ -74,9 +118,28 @@ $(function(){
 
       }
     },
+<<<<<<< HEAD
     startGame: function(){
       this.helpers.initClouds();
       $('#myModal').reveal();
+=======
+    pages: {
+      login: {
+        initPage: function() {}
+      },
+      lobby: {
+        initPage: function() {
+          app.helpers.switchPageTo(app.cachedEls.$lobby);
+        }
+      },
+      game: {
+        initPage: function() {
+          app.helpers.switchPageTo(app.cachedEls.$game);
+          app.helpers.initClouds();
+        }
+      }
+
+>>>>>>> added turn by turn gameplay
     }
 
   }
