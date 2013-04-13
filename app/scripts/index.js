5720b83f57cdc2e72e121b12f8f2abdb79da2f10
Sviatoslav Zalishchuk <scroll.ua@gmail.com>
2013-04-13 17:36:24 +0300

    major app refactoring

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,6 +1,35 @@
-//
-var app = {};
+var app;
 
-app.init = function() {
-  alert('index init');
-};
+$(function(){
+  app = {
+    init: function() {
+      this.helpers.drawTable();
+      this.bindEvents();
+    },
+    bindEvents: function() {
+      $('#container table td span').on('click', function(){
+        var x = $(this).attr('data-x');
+        var y = $(this).attr('data-y');
+      });
+    },
+    // app.points[x+':'+y] = {status:.., user:2}
+    points: {},
+    settings: {
+      boardWidth: 10,
+      boardHeight: 7
+    },
+    helpers: {
+      drawTable: function() {
+        $('#container').append($('<table/>'));
+        for (var x=1; x<=app.settings.boardHeight; x++) {
+          $('#container table').append('<tr data-x='+x+'></tr>');
+          for (var y=1; y<=app.settings.boardWidth; y++) {
+            $('#container table tr:last').append('<td><span></span></td>');
+          }
+        }
+      }
+    }
+
+  }
+  app.init();
+});
